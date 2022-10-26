cc8d20857064e1f41210df5c8da70f535300eacb
Min Park <88412085+hpmp1379@users.noreply.github.com>
2022-10-27 06:36:30 +0900

    1027_update

--- js/test.js
+++ js/test.js
@@ -1,12 +1,18 @@
 $(function(){
     $('header').slideDown(1000);
+    setTimeout(function(){
+        $('.mainTitle h2 span').css({'margin-top':0});
+    },500)
+    setTimeout(function(){
+        $('.mainTitle p span').css({'margin-top':0});
+    },1000)
     $('.logo a img').on('click',function(){
-        $('#sectionScroll').animate({'margin-left': -100+'vw'});
+        $('#sectionScroll').animate({'margin-left': -100+'vw'}, 500);
     })
     $('.carousel-control-prev').on('click',function(){
-        $('#sectionScroll').animate({'margin-left': 0});
+        $('#sectionScroll').animate({'margin-left': 0}, 500);
     })
     $('.carousel-control-next').on('click',function(){
-        $('#sectionScroll').animate({'margin-left': -200+'vw'});
+        $('#sectionScroll').animate({'margin-left': -200+'vw'}, 500);
     })
 })
